37ba1c147d5604cc96047506c4ac4491d4bf5b11
thomasdonlon <thomasdonlonii@gmail.com>
2023-12-12 12:22:18 -0600

    day12 all

--- day12/day12.py
+++ day12/day12.py
@@ -71,9 +71,65 @@ print(f'Sum of all possible combinations: {cum_sum}')
 
 #part 2
 
+known_dict = {}
+
+def is_allowed(springs, test_str):
+	#? are wildcards, X and . must be the same
+	for ch1, ch2 in zip(springs, test_str):
+		if not((ch1 == ch2) or (ch1 == '?')):
+			return False
+	return True
+
+def generate_possible(remaining_str, vals):
+
+	#check memoization
+	if remaining_str + ' ' + str(vals) in known_dict.keys():
+		return known_dict[remaining_str + ' ' + str(vals)]
+	else:
+
+		n_vals = 0
+
+		base_str = ''
+		for i in range(vals[0]):
+			base_str += '#'
+
+		if len(vals) == 1: #this is last value
+			diff = len(remaining_str) - len(base_str)
+			while diff >= 0:
+				tmp = base_str
+				for i in range(diff):
+					tmp += '.'
+				if is_allowed(remaining_str, tmp):
+					n_vals += 1
+
+				base_str = '.' + base_str
+				diff -= 1
+			
+		else: #more values afterwards, recurse
+			base_str += '.'
+
+			diff = len(remaining_str) - len(base_str)
+			#print(diff, vals[1:], sum(vals[1:]) + len(vals[1:]) - 1)
+			while diff >= sum(vals[1:]) + len(vals[1:]) - 1: #minimum length of vals including 1 separating '.'
+				if is_allowed(remaining_str, base_str):
+					n_vals += generate_possible(remaining_str[len(base_str):], vals[1:])
+				base_str = '.' + base_str
+				diff -= 1
+
+		known_dict[remaining_str + ' ' + str(vals)] = n_vals
+		return n_vals
+
 cum_sum = 0
-for j, line in enumerate(data):
+for line in data:
 	line = line.strip().split()
-	cum_sum += n_perms(line[0]*5, [int(i) for i in line[1].split(',')]*5)
+	l = [int(i) for i in line[1].split(',')]
+	lout = []
+	outstr = ''
+	for i in range(5):
+		lout += l.copy()
+		outstr += line[0] + '?'
+	outstr = outstr[:-1] #remove trailing '?'
+	val = generate_possible(outstr, lout)
+	cum_sum += val
 
 print(f'Sum of all possible combinations: {cum_sum}')
